3ebfae390e37fbc15130e6023dbf3aae5d04bec2
Serge ALEXANDRE <sa@broadsoftware.com>
2020-11-20 12:30:16 +0100

    Add support od cluster_issuers for certificate manager

--- k8s/cert_manager/groomer.py
+++ k8s/cert_manager/groomer.py
@@ -15,18 +15,43 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with EzCluster.  If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
 
-from misc import setDefaultInMap
+from misc import setDefaultInMap,ERROR
+
 
 CLUSTER = "cluster"
 K8S="k8s"
 CERT_MANAGER="cert_manager"
 DISABLED = "disabled"
+CERT_MANAGER_ISSUER_BY_ID = "certManaagerIssuerById"
+CONFIG = "config"
+CERT_MANAGER_ISSUERS = "cert_manager_issuers"
+DATA = "data"
+NAME="name"
+ID="id"
+CLUSTER_ISSUERS = "cluster_issuers"
 
+def groomIssuers(model):
+    model[DATA][CERT_MANAGER_ISSUER_BY_ID] = {}
+    for issuer in model[CONFIG][CERT_MANAGER_ISSUERS]:
+        if issuer[ID] in model[DATA][CERT_MANAGER_ISSUER_BY_ID]:
+            ERROR("Cert_manager_issuer of id '{}' is defined twice in configuration file!".format(issuer[ID]))
+        model[DATA][CERT_MANAGER_ISSUER_BY_ID][issuer[ID]] = issuer
+    
+    
 def groom(_plugin, model):
+    groomIssuers(model)
     setDefaultInMap(model[CLUSTER], K8S, {})
     setDefaultInMap(model[CLUSTER][K8S], CERT_MANAGER, {})
     setDefaultInMap(model[CLUSTER][K8S][CERT_MANAGER], DISABLED, False)
     if model[CLUSTER][K8S][CERT_MANAGER][DISABLED]:
         return False
     else:
+        model[DATA][CLUSTER_ISSUERS] = []
+        if CLUSTER_ISSUERS in model[CLUSTER][K8S][CERT_MANAGER]:
+            for issuerDef in model[CLUSTER][K8S][CERT_MANAGER][CLUSTER_ISSUERS]:
+                if issuerDef[ID] not in model[DATA][CERT_MANAGER_ISSUER_BY_ID]:
+                    ERROR("Issuer of id '{}' is not defined in configuration file!".format(issuerDef[ID]))
+                issuer = model[DATA][CERT_MANAGER_ISSUER_BY_ID][issuerDef[ID]]
+                issuer[NAME] = issuerDef[NAME]
+                model[DATA][CLUSTER_ISSUERS].append(issuer)
         return True
